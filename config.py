31badf95c6e6b006affac41030f65d4009f1adca
jayagaming7453-del <jayagaming7453@gmail.com>
2025-09-22 12:55:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("22625485"))
+API_HASH = getenv("d9a1e60445bdbf6626d03fe5aa45f577")
 
-BOT_TOKEN = getenv("BOT_TOKEN", None)
+BOT_TOKEN = getenv("8314453591:AAFF6M3vZwuN_tGUacHMFc-d-K-5KHOGtiQ", None)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("7730034853"))
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/b3d0e737737d67a5bf5a5.jpg")
